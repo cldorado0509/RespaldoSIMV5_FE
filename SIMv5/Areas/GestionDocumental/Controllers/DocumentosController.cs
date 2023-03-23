5ed623e0cce333be79e2524ec9b8df6f667d6cd1
Reinaldo Barco Lopez <reyblop@gmail.com>
2023-03-23 09:05:45 -0500

    Marzo 23 2023

---
+++ SIMv5/Areas/GestionDocumental/Controllers/DocumentosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIM.Areas.GestionDocumental.Controllers
+{
+    public class DocumentosController : Controller
+    {
+        // GET: GestionDocumental/Documentos
+        public ActionResult BuscarDocumentos()
+        {
+            return View();
+        }
+    }
+}
