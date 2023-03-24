3e97b1e82bb1a3b1793639ab0d8aa0f1a8824826
Reinaldo Barco Lopez <reyblop@gmail.com>
2023-03-24 09:27:22 -0500

    we

--- SIMv5/Controllers/UtilidadesApiController.cs
+++ SIMv5/Controllers/UtilidadesApiController.cs
@@ -1205,6 +1205,13 @@
             {
                 if (objData.Indices != null)
                 {
+                    var TraDoc = (from D in dbSIM.TBTRAMITEDOCUMENTO
+                                  where D.ID_DOCUMENTO == objData.IdDocumento
+                                  select new
+                                  {
+                                      D.CODTRAMITE,
+                                      D.CODDOCUMENTO
+                                  }).FirstOrDefault();
                     foreach (Indice indice in objData.Indices)
                     {
                         if (indice.OBLIGA == 1 && (indice.VALOR == null || indice.VALOR == ""))
@@ -1213,7 +1220,7 @@
                         }
                         if (objData.IdDocumento > 1)
                         {
-                            TBINDICEDOCUMENTO indiceDoc = dbSIM.TBINDICEDOCUMENTO.Where(i => i.ID_DOCUMENTO == objData.IdDocumento && i.CODINDICE == indice.CODINDICE).FirstOrDefault();
+                            TBINDICEDOCUMENTO indiceDoc = dbSIM.TBINDICEDOCUMENTO.Where(i => i.CODTRAMITE == TraDoc.CODTRAMITE && i.CODDOCUMENTO == TraDoc.CODDOCUMENTO && i.CODINDICE == indice.CODINDICE).FirstOrDefault();
                             if (indiceDoc != null)
                             {
                                 indiceDoc.VALOR = indice.VALOR ?? "";
