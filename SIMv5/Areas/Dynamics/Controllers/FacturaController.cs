088d27b8dab8ba78cfedbc7104717adb1644eeba
Reinaldo Barco Lopez <reyblop@gmail.com>
2024-03-18 14:13:47 -0500

    Marzo 18 2024 Inicial

---
+++ SIMv5/Areas/Dynamics/Controllers/FacturaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIM.Areas.Dynamics.Controllers
+{
+    public class FacturaController : Controller
+    {
+        // GET: Dynamics/Factura
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
