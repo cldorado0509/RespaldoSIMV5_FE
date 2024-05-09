14a994992249b0b8e627ebfc61ce8628fd72b3d8
Jorge Enrique Estrada Correa <proyectosamva@gmail.com>
2024-05-09 13:18:09 -0500

    Avances

---
+++ SIMv5/Areas/ProcesosJudiciales/DTOs/ActuacionDTO.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SIM.Areas.ProcesosJudiciales.DTOs
+{
+    public class ActuacionDTO
+    {
+        [JsonProperty("actuacionId")]
+        public int ActuacionId { get; set; }
+
+        [JsonProperty("etapa")]
+        public string Etapa { get; set; } = String.Empty;
+
+        [JsonProperty("Concilición")]
+        public bool Conciliacion { get; set; }
+
+        [JsonProperty("valorConciliado")]
+        public float ValorConciliado { get; set; }
+
+        [JsonProperty("finalizado")]
+        public bool Finalizado { get; set; }
+
+        [JsonProperty("fechaFinalizacion")]
+        public DateTime FechaFinalizacion { get; set; }
+
+        [JsonProperty("comiteVerificacion")]
+        public bool ComiteVerificacion { get; set; }
+
+        [JsonProperty("desacato")]
+        public bool Desacato { get; set; }
+
+    }
+}
---
+++ SIMv5/Areas/ProcesosJudiciales/DTOs/DemandantesDTO.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SIM.Areas.ProcesosJudiciales.DTOs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DemandantesDTO
+    {
+        [JsonProperty("demantanteId")]
+        public string demantanteId { get; set; }
+
+        [JsonProperty("identificacion")]
+        public string Identificacion { get; set; } = String.Empty;
+
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; } = String.Empty;
+    }
+}
